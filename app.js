ffae084f815eb583fbf7eac603f33ee57ae3a19d
ivandevyo <ivandevyo@quartsoft.com>
2019-05-02 14:33:25 +0300

    Created all apis

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const favicon = require('static-favicon');
 const logger = require('morgan');
-require('./app_api/models/db')
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+require('./app_api/models/db');
 
-const indexRouter = require('./app_server/routes/index');
-const usersRouter = require('./app_server/routes/users');
-const routesApi = require('./app_api/routes/index')
+const routes = require('./app_server/routes/index');
+const routesApi = require('./app_api/routes/index');
+const users = require('./app_server/routes/users');
 
 const app = express();
 
@@ -15,30 +16,47 @@ const app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
+app.use(favicon());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
 app.use('/api', routesApi);
+app.use('/users', users);
 
-// catch 404 and forward to error handler
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -81,39 +81,119 @@ const theEarth = (function() {
     return locations;
   };
 
-module.exports.locationsCreate = function(req, res) {
-    sendJSONResponse(res, 200, {"status": "success"})
-}
-
-module.exports.locationsReadOne = function(req, res) {
-	if(req.params && req.params.locationid) {
-		Loc
-            .findById(req.params.locationid)
-            .exec(function(err, location) {
-                if(!location) {
-                    sendJSONResponse(res, 404, {
-                        "message": "location not found"
-                    });
-                    return;
-                } else if(err) {
-                    sendJSONResponse(res, 404, err);
-                    return;
-                }
-
-                sendJSONResponse(res, 200, location);
-            });
-	} else {
-		sendJSONResponse(res, 404, {
-			"message": "No locationid in request"
-		});
-	}
-	
-}
-
-module.exports.locationsUpdateOne = function(req, res) {
+  module.exports.locationsCreate = function(req, res) {
+    Loc.create({
+      name: req.body.name,
+      address: req.body.address,
+      facilities: req.body.facilities.split(","),
+      coords: [parseFloat(req.body.lng),
+        parseFloat(req.body.lat)],
+      openingTimes: [{
+        days: req.body.days1,
+        opening: req.body.opening1,
+        closing: req.body.closing1,
+        closed: req.body. closed1
+      }, {
+        days: req.body.days2,
+        opening: req.body.opening2,
+        closing: req.body.closing2,
+        closed: req.body. closed2
+      }]
+    }, function(err, location) {
+      if(err) {
+        sendJSONResponse(res, 400, err);
+      } else {
+        sendJSONResponse(res, 201, location);
+      }
+    });
+  }
+  
+  module.exports.locationsReadOne = function(req, res) {
+    if(req.params && req.params.locationid) {
+      Loc
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if(!location) {
+          sendJSONResponse(res, 404, {
+            "message": "location not found"
+          });
+          return;
+        } else if(err) {
+          sendJSONResponse(res, 404, err);
+          return;
+        }
+  
+        sendJSONResponse(res, 200, location);
+      });
+    } else {
+      sendJSONResponse(res, 404, {
+        "message": "No locationid in request"
+      });
+    }
     
-}
-
-module.exports.locationsDeleteOne = function(req, res) {
-    
+  }
+  
+  module.exports.locationsUpdateOne = function(req, res) {
+    if(!req.params.locationid) {
+      sendJSONResponse(res, 404, {
+        "message": "locationid not found"
+      });
+      return;
+    }
+    Loc
+      .findById(req.params.locationid)
+      .select('-reviews - rating')
+      .exec(function(err, location) {
+        if(!location) {
+          sendJSONResponse(res, 404, {
+            "message": "location not found"
+          });
+          return;
+        } else if(err) {
+          sendJSONResponse(res, 400, err);
+          return;
+        }
+        location.name = req.body.name;
+        location.address = req.body.address;
+        location.facilities = req.body.facilities.split(",");
+        location.coords = [parseFloat(req.body.lng),
+          parseFloat(req.body.lat)];
+        location.openingTimes = [{
+          days: req.body.days1,
+          opening: req.body.opening1,
+          closing: req.body.closing1,
+          closed: req.body. closed1
+        }, {
+          days: req.body.days2,
+          opening: req.body.opening2,
+          closing: req.body.closing2,
+          closed: req.body. closed2
+        }];
+        location.save(function(err, location) {
+          if(err) {
+            sendJSONResponse(res, 404, err);
+          } else {
+            sendJSONResponse(res, 200, location);
+          }
+        });
+      });
+  }
+  
+  module.exports.locationsDeleteOne = function(req, res) {
+    const locationid = req.params.locationid;
+    if(locationid) {
+      Loc
+        .findByIdAndRemove(locationid)
+        .exec(function(err, location) {
+          if(err) {
+            sendJSONResponse(res, 404, err);
+            return;
+          }
+          sendJSONResponse(res, 204, null);
+        })
+    } else {
+      sendJSONResponse(res, 404, {
+        "message": "No locationid"
+      });
+    }
 }
